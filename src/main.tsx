b49ea06cc9ecd23602f77bbe5f08ce2a4361fb15
notkissbe <bencetel1@gmail.com>
2025-01-15 20:01:42 +0100

    hello frontend

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Landing from './Landing/Landing.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Landing/>}>
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
