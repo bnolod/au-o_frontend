6b744f6b0624fee6fb7be94c1db54fad304a57b5
bolond <sadfoolc@gmail.com>
2025-01-15 20:05:03 +0100

    navbar li itemek kozepre

--- src/Landing/Header.tsx
+++ src/Landing/Header.tsx
@@ -44,7 +44,7 @@ export default function Header(){
         >
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
 
-            <li>
+            <li className="flex items-center">
               <a
                 href="#"
                 className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
@@ -52,7 +52,7 @@ export default function Header(){
                 Download our App
               </a>
             </li>
-            <li>
+            <li className="flex items-center">
               <a
                 href="#"
                 className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
@@ -60,7 +60,7 @@ export default function Header(){
                 FAQ
               </a>
             </li>
-            <li>
+            <li className="flex items-center">
               <a
                 href="#"
                 className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
@@ -68,7 +68,7 @@ export default function Header(){
                 About Us
               </a>
             </li>
-            <li>
+            <li className="flex items-center">
               <a
                 href="#"
                 className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
@@ -76,7 +76,7 @@ export default function Header(){
                 Log In
               </a>
             </li>
-            <li>
+            <li className="flex items-center">
               <a
                 href="#"
                 className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
