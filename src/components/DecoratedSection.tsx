98f59321dda6a9ce53e8607ed7f91ddfe9e3fe6c
bnolod <sadfoolc@gmail.com>
2025-01-17 14:08:15 +0100

    hello component

--- src/components/DecoratedSection.tsx
+++ src/components/DecoratedSection.tsx
@@ -10,3 +10,25 @@
 //
 //
 
+import { ReactElement, ReactNode } from "react";
+import ScreenHeightPage from "./ScreenHeightPage";
+
+interface SectionProps {
+  className?: string;
+  innerClassName?: string;
+  children?: ReactNode;
+  navigation?: ReactElement;
+}
+
+export default function DecoratedSection(props: SectionProps) {
+  return (
+    <ScreenHeightPage className={props.className && props.className}>
+      {props.navigation}
+      <div className={props.innerClassName && props.innerClassName}>
+        <img />
+        {props.children}
+        <img />
+      </div>
+    </ScreenHeightPage>
+  );
+}
