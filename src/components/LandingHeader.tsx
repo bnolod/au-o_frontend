30753f847bb7758dfa3d7ee25589cada4210bd34
notkissbe <bencetel1@gmail.com>
2025-01-28 10:06:52 +0100

    header 1/3 -> 2/3

--- src/components/LandingHeader.tsx
+++ src/components/LandingHeader.tsx
@@ -17,7 +17,7 @@ export default function LandingHeader({language = "EN"}:{language?: "EN" | "HU";
             />
           </Link>
         </div>
-        <div className="basis-1/3 items-center p-5"><div className="hidden w-full md:block md:w-auto" id="navbar-default">
+        <div className="basis-2/3 items-center p-5"><div className="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
               <li className="flex items-center">
                 <Link
@@ -53,7 +53,7 @@ export default function LandingHeader({language = "EN"}:{language?: "EN" | "HU";
               </li>
             </ul>
           </div></div>
-        <div className="basis-1/3 flex-"></div>
+        
       </nav>
     </header>
   );
