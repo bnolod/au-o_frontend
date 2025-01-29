588f93a964d65e33d220c20e98eb80a557edfcc8
notkissbe <bencetel1@gmail.com>
2025-01-29 13:44:55 +0100

    landing lookin good

--- src/components/ScrollableDisplay.tsx
+++ src/components/ScrollableDisplay.tsx
@@ -7,21 +7,32 @@ export default function ScrollableDisplay({
 }: {
   className?: string;
   language?: "EN" | "HU";
-}) 
-{
-    const [firstPage,setFirstPage] = useState<boolean>(true);
-    
-    function clickHandler(){
-        setFirstPage(!firstPage);
-    }
- 
-    return (
+}) {
+  const [firstPage, setFirstPage] = useState<boolean>(true);
+
+  function clickHandler() {
+    setFirstPage(!firstPage);
+  }
+
+  return (
     <div
-        onClick={clickHandler}
-      className={className+ " " + (firstPage ? "text-left" : "text-right") + " flex flex-col justify-between bg-backdropPrimary/10 w-full rounded-2xl"}
+      onClick={clickHandler}
+      className={
+        className +
+        " " +
+        (firstPage ? "text-left items-start" : "text-right items-end") +
+        " flex flex-col justify-between bg-backdropPrimary/10 w-full rounded-2xl"
+      }
     >
-      <h3 className="text-4xl">{firstPage ? LandingTexts.title2[language] : LandingTexts.title3[language]}</h3>
-        <hr />
+      <h3 className="text-4xl w-9/12">
+        {firstPage
+          ? LandingTexts.title2[language]
+          : LandingTexts.title3[language]}
+      </h3>
+      <div className="self-center w-full flex justify-center pt-6">
+      <hr className={"w-2/12 mx-1 " + (firstPage ? "border-highlightPrimary" : "border-highlightSecondary")}/>
+      <hr className={"w-2/12 mx-1 " + (!firstPage ? "border-highlightPrimary" : "border-highlightSecondary")}/>
+      </div>
     </div>
   );
 }
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -4,26 +4,27 @@ import { LandingTexts } from "../constants/texts";
 import { useLanguage } from "../contexts/LanguageContext";
 
 export default function LandingPage() {
-  const {language} = useLanguage();
+  const { language } = useLanguage();
 
   return (
     <div className="sm:max-h-screen sm:h-screen bg-backgroundGradient overflow-hidden">
       <LandingHeader></LandingHeader>
       <main className="flex flex-col text-textColor h-full p-16">
         <div className="flex flex-col lg:flex-row justify-center items-center lg:h-1/2">
-          <h1 className="text-7xl">
-            {LandingTexts.title[language]}
-          </h1>
-          <img
-            className="h-full"
-            src="assets/FrontDecoration.svg"
-            alt=""
-          />
+          <h1 className="text-7xl">{LandingTexts.title[language]}</h1>
+          <img className="h-full" src="assets/FrontDecoration.svg" alt="" />
         </div>
-        <div className="flex flex-col lg:flex-row justify-center items-center flex-grow  ">
-          <ScrollableDisplay className="basis-5/12" language={language}/>
-          <div className="flex flex-col flex-grow basis-5/12 text-center bg-backdropPrimary/20">
-            hello
+        <div className="flex flex-col lg:flex-row items-center justify-between flex-grow  ">
+          <ScrollableDisplay className="basis-5/12 p-5" language={language} />
+          <div className="flex flex-col items-center basis-5/1 text-center p-5">
+            <button className="text-4xl dark:bg-highlightPrimary bg-highlightSecondary p-4 rounded-lg w-4/6">
+              {LandingTexts.button[language]}
+            </button>
+            <p>{LandingTexts.mobile[language]}</p>
+            <div className="flex">
+              <img className="w-1/2" src="assets/apple.svg" alt="" />
+              <img className="w-1/2" src="assets/android.svg" alt="android link" />
+            </div>
           </div>
         </div>
       </main>
