8617590a40e4fc3e4936d582a3f9b6db28727b0c
bnolod <sadfoolc@gmail.com>
2025-01-29 14:30:12 +0100

    basis fix

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -10,13 +10,13 @@ export default function LandingPage() {
     <div className="sm:max-h-screen sm:h-screen bg-backgroundGradient overflow-hidden">
       <LandingHeader></LandingHeader>
       <main className="flex flex-col text-textColor h-full p-16">
-        <div className="flex flex-col lg:flex-row justify-center items-center lg:h-1/2">
+        <div className="flex flex-col lg:flex-row justify-center items-center lg:h-1/2 p-4">
           <h1 className="text-7xl basis-5/12">{LandingTexts.title[language]}</h1>
-          <img className="h-full" src="assets/FrontDecoration.svg" alt="" />
+          <img className="basis-7/12 h-full" src="assets/FrontDecoration.svg" alt="" />
         </div>
-        <div className="flex flex-col lg:flex-row items-center justify-center flex-grow  ">
+        <div className="flex flex-col lg:flex-row items-center justify-center flex-grow p-4  ">
           <ScrollableDisplay className="basis-5/12 p-5" language={language} />
-          <div className="flex flex-col items-center basis-5/1 text-center p-5">
+          <div className="flex flex-col items-center basis-7/12 text-center p-5">
             <button className="text-4xl dark:bg-highlightPrimary bg-highlightSecondary p-4 rounded-lg w-4/6">
               {LandingTexts.button[language]}
             </button>
