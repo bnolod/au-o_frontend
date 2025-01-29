e1fadcdaf8d38857572eb37391d2281d0bf12752
bnolod <sadfoolc@gmail.com>
2025-01-29 14:57:10 +0100

    télvind véktzéstk

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -9,15 +9,19 @@ export default function LandingPage() {
   return (
     <div className="sm:max-h-screen sm:h-screen bg-backgroundGradient overflow-hidden">
       <LandingHeader></LandingHeader>
-      <main className="flex flex-col text-textColor h-full p-16">
-        <div className="flex flex-col lg:flex-row justify-center items-center lg:h-1/2 p-4">
-          <h1 className="text-7xl basis-5/12">{LandingTexts.title[language]}</h1>
-          <img className="basis-7/12 h-full" src="assets/FrontDecoration.svg" alt="" />
+      <main className="flex flex-col text-textColor h-full p-24">
+        <div className="flex flex-col lg:flex-row justify-center items-center lg:h-1/2 ">
+          <h1 className="text-6xl font-bold basis-6/12">
+            {LandingTexts.title[language]}
+          </h1>
+          <div className="basis-6/12">
+            <img className="h-full" src="assets/FrontDecoration.svg" alt="" />
+          </div>
         </div>
-        <div className="flex flex-col lg:flex-row items-center justify-center flex-grow p-4  ">
-          <ScrollableDisplay className="basis-5/12 p-5" language={language} />
-          <div className="flex flex-col items-center basis-7/12 text-center p-5">
-            <button className="text-4xl dark:bg-highlightPrimary bg-highlightSecondary p-4 rounded-lg w-4/6">
+        <div className="flex flex-col lg:flex-row items-center flex-grow   ">
+          <ScrollableDisplay className="basis-6/12 h-full p-6 font-medium" language={language} />
+          <div className="flex flex-col items-center basis-6/12 text-center">
+            <button className="text-4xl dark:bg-highlightPrimary bg-highlightSecondary py-4 px-20 font-semibold  rounded-lg ">
               {LandingTexts.button[language]}
             </button>
             <p>{LandingTexts.mobile[language]}</p>
