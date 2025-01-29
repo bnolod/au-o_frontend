d2c2ec8891112014c20012cd66e9ae4f5d57006a
notkissbe <bencetel1@gmail.com>
2025-01-29 14:25:23 +0100

    aki nem lép egyszerre

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -11,10 +11,10 @@ export default function LandingPage() {
       <LandingHeader></LandingHeader>
       <main className="flex flex-col text-textColor h-full p-16">
         <div className="flex flex-col lg:flex-row justify-center items-center lg:h-1/2">
-          <h1 className="text-7xl">{LandingTexts.title[language]}</h1>
+          <h1 className="text-7xl basis-5/12">{LandingTexts.title[language]}</h1>
           <img className="h-full" src="assets/FrontDecoration.svg" alt="" />
         </div>
-        <div className="flex flex-col lg:flex-row items-center justify-between flex-grow  ">
+        <div className="flex flex-col lg:flex-row items-center justify-center flex-grow  ">
           <ScrollableDisplay className="basis-5/12 p-5" language={language} />
           <div className="flex flex-col items-center basis-5/1 text-center p-5">
             <button className="text-4xl dark:bg-highlightPrimary bg-highlightSecondary p-4 rounded-lg w-4/6">
