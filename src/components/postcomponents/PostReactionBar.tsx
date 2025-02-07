e0b660eec46bce0596778f1a683a246d0e04c749
notkissbe <bencetel1@gmail.com>
2025-02-07 15:22:09 +0100

    valami chat hiding de semmiképp nincs kész

--- src/components/postcomponents/PostReactionBar.tsx
+++ src/components/postcomponents/PostReactionBar.tsx
@@ -1,13 +1,31 @@
-export default function PostReactionBar(){
+import { useContext } from "react";
+import { MdAddComment } from "react-icons/md";
+import { openCommentsContext } from "../../pages/MainPage";
+
+export default function PostReactionBar() {
+    let [openComments, setOpenComments] = useContext(openCommentsContext);
+  
     return (
-        <div className="flex text-xs p-3 justify-start">
-        <button className="outline-highlightPrimary bg-backdropSecondary p-2 mx-1 rounded-lg"> 🔥 gomb</button>
-        <button className="outline-highlightPrimary bg-backdropSecondary p-2 mx-1 rounded-lg"> 😎 gomb</button>
-        <button className="outline-highlightPrimary bg-backdropSecondary p-2 mx-1 rounded-lg"> 😍 gomb</button>
-        <div className="justify-self-end flex flex-col flex-grow text-right">
-            <p className="text-highlightPrimary">Tilted Towers</p>
-            <p>2001.09.11</p>
-        </div>
-        </div>
-    )
+    <div className="flex text-xs p-3 justify-between">
+      <button className="outline-highlightPrimary bg-backdropSecondary p-2 mx-1 rounded-lg">
+        {" "}
+        🔥 gomb
+      </button>
+      <button className="outline-highlightPrimary bg-backdropSecondary p-2 mx-1 rounded-lg">
+        {" "}
+        😎 gomb
+      </button>
+      <button className="outline-highlightPrimary bg-backdropSecondary p-2 mx-1 rounded-lg">
+        {" "}
+        😍 gomb
+      </button>
+      <button className="outline-highlightPrimary bg-backdropSecondary p-2 mx-1 rounded-lg flex items-center " onClick={()=>{setOpenComments(!openComments)}}>
+        <MdAddComment/> gomb
+      </button>
+      <div className=" flex flex-col flex-grow text-right">
+        <p className="text-highlightPrimary">Tilted Towers</p>
+        <p>2001.09.11</p>
+      </div>
+    </div>
+  );
 }
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,19 @@
+import { createContext, useContext, useState } from "react";
 import CommentBoard from "../components/commentboard/CommentBoard";
 import Header from "../components/Header";
 import Post from "../components/postcomponents/Post";
 
+export const openCommentsContext = createContext<boolean>(false);
+
 export default function MainPage() {
+
 
 const longText = "Lorem ipsum dolor sit amet, bam bam bam bam bam bam bam bam bam bam bam bam bam bam bam bam bam bam bam bam bam bam bam bam bam bam bam bam consectetur adipiscing elit. Nullam nec purus nec nisl ultricies ultricies. Nullam nec purus nec nisl ultricies ultricies. Nullam nec purus nec nisl ultricies ultricies. Nullam nec purus nec nisl";
 
+  const [openComments, setOpenComments]  = useState<boolean>(false);
+
   return (
+    <openCommentsContext.Provider value={[openComments, setOpenComments]}>
     <div className="bg-backgroundGradient bg-fixed min-h-screen flex flex-col text-textColor">
       <Header />
       <main className="flex flex-col md:flex-row h-full items-center md:items-start justify-center pt-20">
@@ -16,10 +23,11 @@ const longText = "Lorem ipsum dolor sit amet, bam bam bam bam bam bam bam bam ba
           <Post src="examples/profile.png" text=""/>
           <Post src="vite.svg" text={longText}/>
         </div>
-        <div className="md:w-3/12 w-11/12 fixed right-4 flex items-start">
+        <div className={"md:w-3/12 w-11/12 right-4 items-start " + (openComments ? "hidden" : "fixed flex")}>
         <CommentBoard/>
         </div>
       </main>
     </div>
+    </openCommentsContext.Provider>
   );
 }
