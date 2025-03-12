c9335a0f07e976a483eedfc33116d945a8239c3f
notkissbe <bencetel1@gmail.com>
2025-03-12 15:39:41 +0100

    felbe

--- src/components/postcomponents/PostHeader.tsx
+++ src/components/postcomponents/PostHeader.tsx
@@ -3,8 +3,21 @@ import { UserPostResponseType } from '../../lib/types';
 import Avatar from '@mui/material/Avatar';
 import { grey } from '@mui/material/colors';
 import { NavLink } from 'react-router';
+import { useState } from 'react';
+import { Menu, MenuItem } from '@mui/material';
 
 export default function PostHeader({ user }: { user: UserPostResponseType }) {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = anchorEl ? true : false;
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    console.log('clicked');
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+    console.log(anchorEl)
+  };
+
   return (
     <div className="flex basis-2/12 items-center justify-start w-full ">
       <NavLink to={`/profile/${user.id}`} className={'mr-3'}>
@@ -14,7 +27,17 @@ export default function PostHeader({ user }: { user: UserPostResponseType }) {
       </NavLink>
       <p className="txl flex-1 self-center">{user.nickname}</p>
       <div className="justify-self-end flex justify-center text-xl px-4">
-        <MdMoreHoriz size={32} />
+        <button onClick={handleClick}>
+          <MdMoreHoriz size={32} />
+          <Menu
+          id='profleMenu'
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>Profile</MenuItem>
+          </Menu>
+        </button>
       </div>
     </div>
   );
