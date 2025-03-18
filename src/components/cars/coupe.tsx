4dcae9d50a6e66cf750ab0d48aaec450c03d9f0b
pasztor-mark <pasztormarkk7@gmail.com>
2025-03-18 12:00:10 +0100

    feat: car icon svg components

---
+++ src/components/cars/coupe.tsx
@@ -0,0 +1,297 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgCoupe = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlSpace="preserve"
+    width={props.width || 155}
+    height={props.width || 100}
+    viewBox="0 0 135.467 135.467"
+    {...props}
+  >
+
+    <g transform="matrix(1.21388 0 0 1.21388 -16.397 -76.368)">
+      <circle
+        cx={114.106}
+        cy={126.164}
+        r={0.012}
+        style={{
+          fill: '#000',
+          stroke: '#fff',
+          strokeWidth: 0.79375,
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <path
+        d="m96.126 129.04-48.425.038"
+        style={{
+          opacity: 1,
+          fill: '#fff',
+          fillOpacity: 1,
+          fillRule: 'nonzero',
+          stroke: '#fff',
+          strokeWidth: 0.653893,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <path
+        d="M33.753 128.81a.012.076 0 0 1 .011-.076.012.076 0 0 1 .012.076"
+        style={{
+          opacity: 1,
+          fill: 'none',
+          fillOpacity: 1,
+          fillRule: 'nonzero',
+          stroke: '#fff',
+          strokeWidth: 0.79375,
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <path
+        d="m116.58 129.173 4.488-.127 3.516-.766M124.116 117.089c-.06 4.397-.125 5.942-.84 9.35-.256 1.216 1.524.998 1.234 1.777M96.134 129.02a10.38 10.38 0 0 1 4.576-10.476 10.38 10.38 0 0 1 11.431.081 10.38 10.38 0 0 1 4.426 10.54M27.301 129.29a10.38 10.38 0 0 1 4.263-10.77 10.38 10.38 0 0 1 11.583-.118 10.38 10.38 0 0 1 4.483 10.68"
+        style={{
+          opacity: 1,
+          fill: 'none',
+          fillOpacity: 1,
+          fillRule: 'nonzero',
+          stroke: '#fff',
+          strokeWidth: 0.653893,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <path
+        d="M124.103 117.046c-1.429-1.71-3.013-2.386-5.593-2.802-6.426-1.036-11.403-2.056-18.972-2.663-3.393-.273-5.306-.138-5.937-.243"
+        style={{
+          opacity: 1,
+          fill: 'none',
+          fillOpacity: 1,
+          fillRule: 'nonzero',
+          stroke: '#fff',
+          strokeWidth: 0.653893,
+          strokeLinecap: 'round',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <path
+        d="m27.302 129.319-12.77-2.703"
+        style={{
+          opacity: 1,
+          fill: 'none',
+          fillOpacity: 1,
+          fillRule: 'nonzero',
+          stroke: '#fff',
+          strokeWidth: 0.79375,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <path
+        d="m14.471 126.651-.435-1.292-.006-5.288 1.772-1.23.025-4.687-.533-1.536-.061-.046 3.656-1.555c13.135-4.409 35.629-9.754 55.067-7.656l16.755 7.008 2.96.942"
+        style={{
+          opacity: 1,
+          fill: 'none',
+          fillOpacity: 1,
+          fillRule: 'nonzero',
+          stroke: '#fff',
+          strokeWidth: 0.653893,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <path
+        d="m123.696 117.093-3.575-.42-5.298.434 3.031 1.837c1.675.712 4.831.718 5.9.129"
+        style={{
+          fill: 'none',
+          stroke: '#fff',
+          strokeWidth: 0.435929,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+          markerStart: 'url(#coupe_svg__a)',
+          markerEnd: 'url(#coupe_svg__b)',
+        }}
+      />
+      <path
+        d="m16.881 113.327 2.474.477 1.202 3.987-3.302.154"
+        style={{
+          fill: 'none',
+          stroke: '#fff',
+          strokeWidth: 0.529167,
+          strokeLinecap: 'round',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+          markerStart: 'url(#coupe_svg__c)',
+          markerEnd: 'url(#coupe_svg__c)',
+        }}
+      />
+      <path
+        d="M49.526 127.297H94.1M15.525 124.241l10.253 2.05"
+        style={{
+          fill: 'none',
+          stroke: '#fff',
+          strokeWidth: 0.435929,
+          strokeLinecap: 'round',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <path
+        d="M117.93 126.55c2.176.022 4.117-.469 4.117-.469"
+        style={{
+          fill: 'none',
+          stroke: '#fff',
+          strokeWidth: 0.435929,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <path
+        d="m18.855 115.604-1.297-.023"
+        style={{
+          fill: 'none',
+          stroke: '#fff',
+          strokeWidth: 0.529167,
+          strokeLinecap: 'round',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+          markerStart: 'url(#coupe_svg__d)',
+          markerEnd: 'url(#coupe_svg__c)',
+        }}
+      />
+      <path
+        d="m32.335 108.227 8.81 3.046H91.12"
+        style={{
+          fill: 'none',
+          stroke: '#fff',
+          strokeWidth: 0.435929,
+          strokeLinecap: 'round',
+          strokeMiterlimit: 3.9,
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <circle
+        cx={37.444}
+        cy={126.325}
+        r={7.976}
+        style={{
+          fill: 'none',
+          stroke: '#fff',
+          strokeWidth: 0.653893,
+          strokeLinecap: 'butt',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <circle
+        cx={37.444}
+        cy={126.325}
+        r={6.746}
+        style={{
+          fill: 'none',
+          stroke: '#fff',
+          strokeWidth: 0.435929,
+          strokeLinecap: 'butt',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <g
+        style={{
+          stroke: '#fff',
+          strokeOpacity: 1,
+        }}
+        transform="translate(-.026 -.741)"
+      >
+        <circle
+          cx={106.39}
+          cy={127.298}
+          r={7.976}
+          style={{
+            fill: 'none',
+            stroke: '#fff',
+            strokeWidth: 0.653893,
+            strokeLinecap: 'butt',
+            strokeDasharray: 'none',
+            strokeOpacity: 1,
+          }}
+        />
+        <circle
+          cx={106.39}
+          cy={127.298}
+          r={6.746}
+          style={{
+            fill: 'none',
+            stroke: '#fff',
+            strokeWidth: 0.553492,
+            strokeLinecap: 'butt',
+            strokeDasharray: 'none',
+            strokeOpacity: 1,
+          }}
+        />
+      </g>
+      <g
+        style={{
+          stroke: '#fff',
+          strokeOpacity: 1,
+        }}
+        transform="translate(-8.226 -.414)"
+      >
+        <path
+          d="M68.385 115.367h5.1"
+          style={{
+            fill: 'none',
+            stroke: '#fff',
+            strokeWidth: 0.871857,
+            strokeLinecap: 'round',
+            strokeDasharray: 'none',
+            strokeOpacity: 1,
+          }}
+        />
+        <ellipse
+          cx={70.88}
+          cy={115.367}
+          rx={1.366}
+          ry={0.739}
+          style={{
+            fill: 'none',
+            fillOpacity: 1,
+            stroke: '#fff',
+            strokeWidth: 0.687739,
+            strokeLinecap: 'round',
+            strokeDasharray: 'none',
+            strokeOpacity: 1,
+          }}
+        />
+      </g>
+      <path
+        d="m54.751 104.015.738 6.633"
+        style={{
+          fill: 'none',
+          fillOpacity: 1,
+          stroke: '#fff',
+          strokeWidth: 0.435929,
+          strokeLinecap: 'round',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+    </g>
+  </svg>
+);
+export default SvgCoupe;
---
+++ src/components/cars/suv.tsx
@@ -0,0 +1,315 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgSuv = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlSpace="preserve"
+    width={props.width || 155}
+    height={props.width || 100}
+    viewBox="0 0 135.467 135.467"
+    {...props}
+  >
+
+    <g transform="matrix(1.21388 0 0 1.21388 -16.675 -73.638)">
+      <circle
+        cx={112.532}
+        cy={125.342}
+        r={0.014}
+        style={{
+          fill: '#000',
+          stroke: props.stroke,
+          strokeWidth: props.strokeWidth as number * 0.913163,
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <path
+        d="m91.847 128.651-38.596.043"
+        style={{
+          opacity: 1,
+          fill: '#fff',
+          fillOpacity: 1,
+          fillRule: 'nonzero',
+          stroke: props.stroke,
+          strokeWidth: props.strokeWidth as number * 0.752266,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+     
+      <path
+        d="m115.38 128.804 5.162-.146 4.045-.882M124.049 114.902c-.07 5.059-.144 6.836-.967 10.756-.294 1.4 1.754 1.149 1.42 2.045M91.857 128.627a11.94 11.94 0 0 1 5.264-12.052 11.94 11.94 0 0 1 13.151.094 11.94 11.94 0 0 1 5.092 12.126M29.751 128.923a11.94 11.94 0 0 1 4.905-12.39 11.94 11.94 0 0 1 13.325-.137 11.94 11.94 0 0 1 5.157 12.288"
+        style={{
+          opacity: 1,
+          fill: 'none',
+          fillOpacity: 1,
+          fillRule: 'nonzero',
+          stroke: props.stroke,
+          strokeWidth: props.strokeWidth as number * 0.752266,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <path
+        d="M124.034 114.852c-4.08-4.885-26.253-7.54-26.938-7.786-.605-.218-22.352-9.355-23.213-10.15"
+        style={{
+          opacity: 1,
+          fill: 'none',
+          fillOpacity: 1,
+          fillRule: 'nonzero',
+          stroke: props.stroke,
+          strokeWidth: props.strokeWidth as number * 0.752266,
+          strokeLinecap: 'round',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <path
+        d="m29.753 128.956-14.692-3.11"
+        style={{
+          opacity: 1,
+          fill: 'none',
+          fillOpacity: 1,
+          fillRule: 'nonzero',
+          stroke: props.stroke,
+          strokeWidth: props.strokeWidth as number * 0.913163,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <path
+        d="m14.991 125.887-.5-1.487-.007-6.083 1.932-1.486.03-5.39-.613-1.768 4.68-11.253-2.99.705c16.158-4.134 38.462-3.345 56.363-2.212"
+        style={{
+          opacity: 1,
+          fill: 'none',
+          fillOpacity: 1,
+          fillRule: 'nonzero',
+          stroke: props.stroke,
+          strokeWidth: props.strokeWidth as number * 0.752266,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <path
+        d="m123.566 114.906-4.113-.482-6.096.499 3.488 2.113c1.927.82 5.558.826 6.787.148"
+        style={{
+          fill: 'none',
+          stroke: props.stroke,
+          strokeWidth: props.strokeWidth as number * 0.501511,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <path
+        d="m17.764 110.558 2.846.549 1.383 4.587-3.8.177"
+        style={{
+          fill: 'none',
+          stroke: props.stroke,
+          strokeWidth: props.strokeWidth as number * 0.608776,
+          strokeLinecap: 'round',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <path
+        d="M55.907 126.646h33.61M16.204 123.114l11.795 2.36"
+        style={{
+          fill: 'none',
+          stroke: props.stroke,
+          strokeWidth: props.strokeWidth as number * 0.501511,
+          strokeLinecap: 'round',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <path
+        d="M116.933 125.785c2.502.027 4.735-.538 4.735-.538"
+        style={{
+          fill: 'none',
+          stroke: props.stroke,
+          strokeWidth: props.strokeWidth as number * 0.501511,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <path
+        d="m20.035 113.178-1.493-.027"
+        style={{
+          fill: 'none',
+          stroke: props.stroke,
+          strokeWidth: props.strokeWidth as number * 0.608776,
+          strokeLinecap: 'round',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <path
+        d="m21.62 106.761 21.644 1.45 30.025-.206 23.262-.94"
+        style={{
+          fill: 'none',
+          stroke: props.stroke,
+          strokeWidth: props.strokeWidth as number * 0.501511,
+          strokeLinecap: 'round',
+          strokeMiterlimit: 3.9,
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <circle
+        cx={41.509}
+        cy={127.462}
+        r={9.176}
+        style={{
+          fill: 'none',
+          stroke: props.stroke,
+          strokeWidth: props.strokeWidth as number * 0.752266,
+          strokeLinecap: 'butt',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <circle
+        cx={41.509}
+        cy={127.462}
+        r={7.761}
+        style={{
+          fill: 'none',
+          stroke: props.stroke,
+          strokeWidth: props.strokeWidth as number * 0.501511,
+          strokeLinecap: 'butt',
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+      <g
+        style={{
+          stroke: props.stroke,
+          strokeOpacity: 1,
+        }}
+        transform="translate(-18.68 -18.705)scale(1.15044)"
+      >
+        <circle
+          cx={106.39}
+          cy={127.298}
+          r={7.976}
+          style={{
+            fill: 'none',
+            stroke: props.stroke,
+            strokeWidth: props.strokeWidth as number * 0.653893,
+            strokeLinecap: 'butt',
+            strokeDasharray: 'none',
+            strokeOpacity: 1,
+          }}
+        />
+        <circle
+          cx={106.39}
+          cy={127.298}
+          r={6.746}
+          style={{
+            fill: 'none',
+            stroke: props.stroke,
+            strokeWidth: props.strokeWidth as number * 0.553492,
+            strokeLinecap: 'butt',
+            strokeDasharray: 'none',
+            strokeOpacity: 1,
+          }}
+        />
+      </g>
+      <g
+        style={{
+          stroke: props.stroke,
+          strokeOpacity: 1,
+        }}
+        transform="translate(-6.702 -19.911)scale(1.15044)"
+      >
+        <path
+          d="M68.385 115.367h5.1"
+          style={{
+            fill: 'none',
+            stroke: props.stroke,
+            strokeWidth: props.strokeWidth as number * 0.871857,
+            strokeLinecap: 'round',
+            strokeDasharray: 'none',
+            strokeOpacity: 1,
+          }}
+        />
+        <ellipse
+          cx={70.88}
+          cy={115.367}
+          rx={1.366}
+          ry={0.739}
+          style={{
+            fill: 'none',
+            fillOpacity: 1,
+            stroke: props.stroke,
+            strokeWidth: props.strokeWidth as number * 0.687739,
+            strokeLinecap: 'round',
+            strokeDasharray: 'none',
+            strokeOpacity: 1,
+          }}
+        />
+      </g>
+      <g
+        style={{
+          stroke: props.stroke,
+          strokeOpacity: 1,
+        }}
+        transform="translate(-27.995 -19.809)scale(1.15044)"
+      >
+        <path
+          d="M68.385 115.367h5.1"
+          style={{
+            fill: 'none',
+            stroke: props.stroke,
+            strokeWidth: props.strokeWidth as number * 0.871857,
+            strokeLinecap: 'round',
+            strokeDasharray: 'none',
+            strokeOpacity: 1,
+          }}
+        />
+        <ellipse
+          cx={70.88}
+          cy={115.367}
+          rx={1.366}
+          ry={0.739}
+          style={{
+            fill: 'none',
+            fillOpacity: 1,
+            stroke: props.stroke,
+            strokeWidth: props.strokeWidth as number * 0.687739,
+            strokeLinecap: 'round',
+            strokeDasharray: 'none',
+            strokeOpacity: 1,
+          }}
+        />
+      </g>
+      <path
+        d="m65.193 96.588.027 11.361M41.353 96.588l2.07 11.716M24.57 98.005l-2.943 8.827"
+        style={{
+          fill: 'none',
+          fillOpacity: 1,
+          stroke: props.stroke,
+          strokeWidth: props.strokeWidth as number * 0.653893,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeMiterlimit: 3.9,
+          strokeDasharray: 'none',
+          strokeOpacity: 1,
+        }}
+      />
+    </g>
+  </svg>
+);
+export default SvgSuv;
