fd3648ed8ef2cb1d5daf9dd0dc1d3cab1bc51817
bnolod <sadfoolc@gmail.com>
2025-03-19 15:46:46 +0100

    message board + group feed fix

--- src/components/messages/MessageBoard.tsx
+++ src/components/messages/MessageBoard.tsx
@@ -22,7 +22,8 @@ export default function MessageBoard({ msgOnClick }: { msgOnClick?: boolean }) {
       const existingIndex = prevMessages.findIndex((msg) => {
         // Check if the incoming message is from or to the authenticated user
         return (
-          (msg.username === incomingMessage.username && incomingMessage.message.recipient.username === user?.username) ||
+          (msg.username === incomingMessage.username &&
+            incomingMessage.message.recipient.username === user?.username) ||
           (msg.username === incomingMessage.message.recipient.username && incomingMessage.username === user?.username)
         );
       });
@@ -30,17 +31,16 @@ export default function MessageBoard({ msgOnClick }: { msgOnClick?: boolean }) {
       if (existingIndex !== -1) {
         // Update the existing conversation
         return prevMessages.map((msg, index) =>
-          index === existingIndex
-            ? { ...msg, message: incomingMessage.message, active: incomingMessage.active }
-            : msg
+          index === existingIndex ? { ...msg, message: incomingMessage.message, active: incomingMessage.active } : msg
         );
       } else {
         // If the conversation does not exist, add it to the top
         const formattedMessage = incomingMessage;
-        if(formattedMessage.message.user.username === user?.username) {
+        if (formattedMessage.message.user.username === user?.username) {
           formattedMessage.username = formattedMessage.message.recipient.username;
-          formattedMessage.nickname = formattedMessage.message.recipient.nickname
+          formattedMessage.nickname = formattedMessage.message.recipient.nickname;
           formattedMessage.id = formattedMessage.message.recipient.id;
+          formattedMessage.profileImg = formattedMessage.message.recipient.profileImg;
         }
         return [formattedMessage, ...prevMessages];
       }
@@ -53,7 +53,7 @@ export default function MessageBoard({ msgOnClick }: { msgOnClick?: boolean }) {
     let sub: { unsubscribe: () => void } | null = null;
     if (user && stompClient && stompClient.connected) {
       sub = stompClient.subscribe(`/user/queue/notifications/`, (msg: { body: string }) => {
-        console.log("helloo");
+        console.log('helloo');
         const incomingMessage = JSON.parse(msg.body) as LatestMessage;
         handleIncomingMessage(incomingMessage);
       });
@@ -67,18 +67,17 @@ export default function MessageBoard({ msgOnClick }: { msgOnClick?: boolean }) {
   }, [user, stompClient]);
 
   return (
-
     <div className="w-full gap-2 flex flex-col h-full mb-4 overflow-y-scroll p-4">
-      <h1 className='text-2xl pb-4 font-semibold text-textColor/50'>Latest messages</h1>
-      {latestMessages.map((item) => (
-        msgOnClick ?
-          <NavLink className='hover:opacity-50 active:opacity-75' to={`/messages/${item.id}`} key={item.username}>
+      <h1 className="text-2xl pb-4 font-semibold text-textColor/50">Latest messages</h1>
+      {latestMessages.map((item) =>
+        msgOnClick ? (
+          <NavLink className="hover:opacity-50 active:opacity-75" to={`/messages/${item.id}`} key={item.username}>
             <LatestMessageItem latestMessage={item} />
           </NavLink>
-          :
+        ) : (
           <LatestMessageItem key={item.username} latestMessage={item} />
-      ))}
+        )
+      )}
     </div>
-
   );
 }
